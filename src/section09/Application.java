1acb5b8bb1b043de4de33948f74b3e4aadcacd05
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-08-12 10:03:58 -0300

    feat: Using encapsulation

---
+++ src/section09/Application.java
@@ -0,0 +1,48 @@
+package section09;
+
+import section09.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+
+        Product product = new Product(name, price);
+
+        product.setName("Computer");
+        System.out.println("Updated name product: " + product.getName());
+        product.setPrice(4500.00);
+        System.out.println("Updated price product: " + product.getPrice());
+        System.out.println("Quantity product: " + product.getQuantity());
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+    }
+}
