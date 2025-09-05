ae5149154a4d7440e0f34c26e0b939e333e25990
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-09-05 09:43:01 -0300

    feat: Upcasting and downcasting

--- src/section13/inheritance/Application.java
+++ src/section13/inheritance/Application.java
@@ -1,11 +1,41 @@
 package section13.inheritance;
 
+import section13.inheritance.entities.Account;
 import section13.inheritance.entities.BusinessAccount;
+import section13.inheritance.entities.SavingsAccounts;
 
 public class Application {
 
     public static void main(String[] args) {
-        BusinessAccount account = new BusinessAccount();
+
+
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1001, "Maria", 0.0 , 500.0);
+
+        // UPCASTING
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccounts(1004, "Anna", 0.0, 0.01);
+
+        // DOWNCASTING
+
+        BusinessAccount bacc4 = (BusinessAccount) acc2;
+        bacc4.loan(100.0);
+
+        // BusinessAccount bacc5 = (BusinessAccount) acc3;
+
+        if (acc3 instanceof BusinessAccount) {
+            BusinessAccount bacc5 = (BusinessAccount) acc3;
+            bacc5.loan(200.0);
+            System.out.print("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccounts) {
+            SavingsAccounts bacc5 = (SavingsAccounts) acc3;
+            bacc5.updateBalance();
+            System.out.print("Update!");
+        }
 
     }
 
---
+++ src/section13/inheritance/entities/SavingsAccounts.java
@@ -0,0 +1,27 @@
+package section13.inheritance.entities;
+
+public class SavingsAccounts extends Account {
+
+    private Double interestingRate;
+
+    public SavingsAccounts() {
+        super();
+    }
+
+    public SavingsAccounts(Integer number, String holder, Double balance, Double interestingRate) {
+        super(number, holder, balance);
+        this.interestingRate = interestingRate;
+    }
+
+    public Double getInterestingRate() {
+        return interestingRate;
+    }
+
+    public void setInterestingRate(Double interestingRate) {
+        this.interestingRate = interestingRate;
+    }
+
+    public void updateBalance() {
+        balance += balance * interestingRate;
+    }
+}
