fd51429e15bdd4bc66b3f98ee3fc0f8bd2ff31ce
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-09-30 09:10:35 -0300

    feat: Generics

---
+++ src/section17/generics/Application.java
@@ -0,0 +1,25 @@
+package section17.generics;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("How many values: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            ps.addValue(sc.nextInt());
+        }
+
+        ps.print();
+
+        System.out.print("First: " + ps.first());
+
+    }
+}
